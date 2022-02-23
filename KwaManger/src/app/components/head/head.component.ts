0af306ff2e83945d25eb76edb2589133147fb694
IlangoRemi <ilangoremi@gmail.com>
2022-02-23 16:34:57 +0100

    bouton + dans component head, liaison entre barre de recherche et bouton

--- KwaManger/src/app/components/head/head.component.ts
+++ KwaManger/src/app/components/head/head.component.ts
@@ -8,8 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class HeadComponent implements OnInit {
 
   constructor() { }
-
   ngOnInit(): void {
+    tags: this.fb.array([])
   }
 
 }
