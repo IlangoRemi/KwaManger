001b943a1bdcb34c2748a613c8192d0d31e96fb8
Hugo COUTANT <hugo12001111@gmail.com>
2022-03-16 14:51:44 +0100

    update hugo 16/03

--- KwaManger/src/app/components/recettes/recettes.component.ts
+++ KwaManger/src/app/components/recettes/recettes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+//import {writeJsonFile} from 'write-json-file';
 
 @Component({
   selector: 'app-recettes',
@@ -8,6 +9,10 @@ import { Component, OnInit } from '@angular/core';
 export class RecettesComponent implements OnInit {
 
   static infoNutri : boolean = false;
+  apiKey = "&app_key=7e75073a08906ea21a48e21d07af238b";
+  apiId = "&app_id=e323e869";
+  maxTime = "&time=30";
+  maxIngreds = `&ingr=10`;
 
   constructor() { }
 
@@ -21,4 +26,47 @@ export class RecettesComponent implements OnInit {
   static getInfoNutri(): boolean {
     return this.infoNutri;
   }
+
+  test(): void {
+    this.fetchRecipes("carrot", "chicken");
+  }
+
+  requeteApi = async(url: string) => {
+  
+    return fetch(url).then(response => {
+      if(response.status >=200 && response.status < 300){
+        return response.json();
+      }else{
+        return new Error('Erreur serveur');
+      }
+    })
+    .catch(error => {
+      alert(error);
+      throw new Error('Erreur charge,ent');
+    });
+  };
+
+  fetchRecipes = async (...ingredients: String[]) => {
+    const mappedIngreds = ingredients
+      .map((ingredient, idx) => {
+        if (idx < ingredients.length - 1) {
+          return ingredient + "+";
+        } else {
+          return ingredient;
+        }
+      })
+      .join("");
+  
+    const url = "https://api.edamam.com/search?q=" + mappedIngreds + this.maxIngreds + this.maxTime + this.apiId + this.apiKey;
+    console.log("Voici l'url : " + url);
+
+    let recipes = this.requeteApi(url);
+    console.log(recipes);
+    //await writeJsonFile('recette.json', recipes);
+
+    //fetch url
+    /*const recipes = res.data;
+    console.log(recipes);*/
+    //ajout dans une liste ?
+  };
 }
